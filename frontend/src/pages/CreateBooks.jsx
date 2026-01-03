6e5039471bd408c522bb3d27ef2d6a4306e5605b
parihargaurav <parihargaurav527@gmail.com>
2026-01-03 15:02:23 +0530

    feat/style: Update styles for CreateBook and EditBook components; enhance layout and color scheme for better UI

--- frontend/src/pages/CreateBooks.jsx
+++ frontend/src/pages/CreateBooks.jsx
@@ -52,48 +52,48 @@ const CreateBooks = () => {
 
 
   return (
-    <div className='p-4'>
+    <div className=' p-4 px-50 mr-50'>
       <BackButton />
       <h1 className='text-3xl my-4'>Create Book</h1>
       {loading ? <Spinner /> : ''}
-      <div className='flex flex-col border-2 border-sky-400 rounded-xl w-[600px] p-4 mx-auto'>
+      <div className='flex flex-col border-2 border-sky-400 bg-zinc-50 rounded-xl w-[600px] p-4 mx-auto'>
         <div className='my-4'>
-          <label className='text-xl mr-4 text-gray-500'>Title</label>
+          <label className='text-xl mr-4  text-black'>Title</label>
           <input
             type='text'
             value={title}
             onChange={(e) => setTitle(e.target.value)}
-            className='border-2 border-gray-500 px-4 py-2 w-full'
+            className='border-2 border-black px-4 py-2 w-full'
           />
         </div>
         <div className='my-4'>
-          <label className='text-xl mr-4 text-gray-500'>Author</label>
+          <label className='text-xl mr-4 text-black'>Author</label>
           <input
             type='text'
             value={author}
             onChange={(e) => setAuthor(e.target.value)}
-            className='border-2 border-gray-500 px-4 py-2  w-full '
+            className='border-2 border-gray-950 px-4 py-2  w-full '
           />
         </div>
         <div className='my-4'>
-          <label className='text-xl mr-4 text-gray-500'>Price</label>
+          <label className='text-xl mr-4 text-black'>Price</label>
           <input
             type='number'
             value={price}
             onChange={(e) => setPrice(e.target.value)}
-            className='border-2 border-gray-500 px-4 py-2  w-full '
+            className='border-2 border-black px-4 py-2  w-full text-black '
           />
         </div>
         <div className='my-4'>
-          <label className='text-xl mr-4 text-gray-500'>Publish Year</label>
+          <label className='text-xl mr-4 text-black'>Publish Year</label>
           <input
             type='number'
             value={publishYear}
             onChange={(e) => setPublishYear(e.target.value)}
-            className='border-2 border-gray-500 px-4 py-2  w-full '
+            className='border-2 border-black px-4 py-2  w-full '
           />
         </div>
-        <button className='p-2 bg-sky-300 m-8' onClick={handleSaveBook}>
+        <button className='p-2 bg-sky-300 m-8 text-white' onClick={handleSaveBook}>
           Save
         </button>
       </div>
--- frontend/src/pages/EditBook.jsx
+++ frontend/src/pages/EditBook.jsx
@@ -63,11 +63,11 @@ const EditBook = () => {
   };
 
   return (
-    <div className="p-4">
+    <div className="p-4 px-60 mr-60">
       <BackButton />
       <h1 className="text-3xl my-4">Edit Book</h1>
       {loading ? <Spinner /> : ""}
-      <div className="flex flex-col border-2 border-sky-400 rounded-xl w-[600px] p-4 mx-auto">
+      <div className="flex flex-col border-2 border-black rounded-xl w-[600px] p-4 mx-auto bg-white">
         <div className="my-4">
           <label className="text-xl mr-4 text-gray-500">Title</label>
           <input
@@ -104,7 +104,7 @@ const EditBook = () => {
             className="border-2 border-gray-500 px-4 py-2  w-full "
           />
         </div>
-        <button className="p-2 bg-sky-300 m-8" onClick={handleEditBook}>
+        <button className="p-2 bg-sky-300 m-8 text-white" onClick={handleEditBook}>
           Save
         </button>
       </div>
